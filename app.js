bb77ffa6edadc169cb86cc813fa5c267e14bd561
Abhin Rustagi <rustagi.abhin@gmail.com>
2020-08-25 07:38:24 +0530

    Level 2: Encryption

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -14,6 +16,30 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/userdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connection established to the database.");
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"]
+});
+
+const User = new mongoose.model("User", userSchema);
+
 app.route("/")
   .get(function(req, res){
     res.render("home");
@@ -22,11 +48,41 @@ app.route("/")
 app.route("/login")
   .get(function(req, res){
     res.render("login");
+  })
+  .post(function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, found){
+      if(!err) {
+        if(found){
+          if(found.password === password){
+            res.render("secrets");
+          }
+        }
+      } else {
+        console.log(err);
+      }
+    });
   });
 
 app.route("/register")
   .get(function(req, res){
     res.render("register");
+  })
+  .post(function(req, res){
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+
+    newUser.save(function(err){
+      if(err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
 app.listen(8888, function(){
